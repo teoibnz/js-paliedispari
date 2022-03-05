ef83aceaaab10c0d1ccf238d7d044bc791b291dc
Matteo Lopez <matteolopezbb@gmail.com>
2022-03-05 12:42:48 +0100

    finalizzazione esercizio pali e dispari

--- main.js
+++ main.js
@@ -33,28 +33,55 @@
 
 
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-const oddOrEven = prompt('scegli se pari o dispari');
-console.log('L\'utente ha scelto' + '  ' + oddOrEven);
-const userNumber = parseInt(prompt('inserisci un numero da 1 a 5' ));
-console.log('L\'utente ha scelto'+ '  ' + userNumber);
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-function randomNumber () {
-    let num = Math.floor(Math.random(1) * 6);
-    return num;
+let oddOrEven = "";
+
+while (oddOrEven != "pari" && oddOrEven != 'dispari'){
+    oddOrEven = prompt('scegli se pari o dispari');
+    console.log('L\'utente ha scelto' + '  ' + oddOrEven);
 };
 
-console.log("il pc ha scelto il numero " + randomNumber());
+
+let userNumber = 0;
+
+while (userNumber < 1 || userNumber > 5){
+    userNumber = parseInt(prompt('inserisci un numero da 1 a 5' ));
+    console.log('L\'utente ha scelto'+ '  ' + userNumber);
+}
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+function randomInt (minValue, maxValue){
+	if ( isNaN (parseInt (minValue)) ||  (isNaN  (parseInt(maxValue)))) {
+	console.error("randomInt(min, max) need two numbers as argument");
+}
+	return (Math.floor(Math.random() * ((maxValue + 1) - minValue) + minValue));
+
+};
+
+
+const aINumber = randomInt(1,5);
+
+console.log("il pc ha scelto il numero " + aINumber);
 
 // Sommiamo i due numeri
 
-let pointSum = userNumber + randomNumber();
+let pointSum = userNumber + aINumber;
 console.log('la somma è ' + pointSum);
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
-function evenOrOdd (num){
-    num % 2 == 0;
-    return true;
-};
+function isEven(number){
+    if ( number % 2 == 0 ){
+        return true;
+    }
+    return false;
+}
 // Dichiariamo chi ha vinto.
+
+
+if ((oddOrEven == 'pari' && isEven(pointSum)) ||
+    (oddOrEven == 'dispari') && !isEven(pointSum)) {
+        console.log('Hai vinto!')
+    } else {
+        console.log('Hai perso!')
+    }
 
